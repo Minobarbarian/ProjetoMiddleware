e1b66af6ccf68c424d812c4006cf9ecddf4fb761
Jacpc <tonho_kun@ymail.com>
2024-11-18 08:36:31 -0300

    Identification pattern Lookup

---
+++ src/br/ufrn/imd/patterns/Lookup.java
@@ -0,0 +1,24 @@
+package br.ufrn.imd.patterns;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lookup {
+	private static final Map<String, Object> lookupTable = new HashMap<>();
+	
+	public static void registerObject(String name, Object object) {
+        lookupTable.put(name, object);
+    }
+
+    public static void registerObject(ObjectId objectId, Object object) {
+        lookupTable.put(objectId.toString(), object);
+    }
+
+    public static Object getObject(String nameOrId) {
+        return lookupTable.get(nameOrId);
+    }
+
+    public static boolean contains(String nameOrId) {
+        return lookupTable.containsKey(nameOrId);
+    }
+}
