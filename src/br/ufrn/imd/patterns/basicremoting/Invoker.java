51ff0f0432c70179e4e3f0e19cf40fdd15bd134a
joao.antonio.costa.119 <joao.antonio.costa.119@LA30712060252.imd.local>
2024-11-18 11:58:01 -0300

    Trying JSON

--- src/br/ufrn/imd/patterns/basicremoting/Invoker.java
+++ src/br/ufrn/imd/patterns/basicremoting/Invoker.java
@@ -1,15 +1,28 @@
 package br.ufrn.imd.patterns.basicremoting;
 
+import org.json.JSONObject;
 import java.lang.reflect.Method;
 
 public class Invoker {
-	public static String invoke(Method method, Object instance, String requestBody) {
+	public static JSONObject invoke(Method method, JSONObject instance, String requestBody) {
+		JSONObject response = new JSONObject();
 		try {
-			Object argument = Marshaller.unmarshall(requestBody, method.getParameterTypes()[0]);
-            Object result = method.invoke(instance, argument);
-            return Marshaller.marshall(result);
+			if(instance == null) {
+				response.put("error", true);
+				response.put("code", "404");
+				response.put("message", "Instance not provided for invocation.");
+                return response;
+			}
+			
+			JSONObject requestJson = new JSONObject(requestBody);
+			Class<?> parameterType = method.getParameterTypes()[0];
+			Object argument = Marshaller.unmarshall(requestJson.toString(), parameterType);
+			
+            Object result = method.invoke(instance.toMap(), argument);
+            response = new JSONObject(Marshaller.marshall(result));
 		} catch (Exception e) {
-			return RemotingError.handleError(e);
+			response = new JSONObject(RemotingError.handleError(e));
 		}
+		return response;
 	}
 }
