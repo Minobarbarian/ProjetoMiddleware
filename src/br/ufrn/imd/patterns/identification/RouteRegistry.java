5b6c57e9939c90a099b32ac268431d02422120b3
joao.antonio.costa.119 <joao.antonio.costa.119@LA30712060252.imd.local>
2024-11-18 11:30:15 -0300

    no comment

--- src/br/ufrn/imd/patterns/identification/RouteRegistry.java
+++ src/br/ufrn/imd/patterns/identification/RouteRegistry.java
@@ -8,17 +8,14 @@ import java.util.Set;
 public class RouteRegistry {
 	private static final Map<String, Method> routeMethods = new HashMap<>();
 
-    // Register a method for a specific route
     public static void registerRoute(String route, Method method) {
         routeMethods.put(route, method);
     }
 
-    // Retrieve the method associated with a route
     public static Method getMethod(String route) {
         return routeMethods.get(route);
     }
 
-    // Get all registered routes
     public static Set<String> getRoutes() {
         return routeMethods.keySet();
     }
