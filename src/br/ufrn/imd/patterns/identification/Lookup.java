74fbddd91fd50a6d1a7bf8ef6fe641485f03ed34
Jacpc <tonho_kun@ymail.com>
2024-12-04 08:14:03 -0300

    Check for Put and Delete

--- src/br/ufrn/imd/patterns/identification/Lookup.java
+++ src/br/ufrn/imd/patterns/identification/Lookup.java
@@ -2,8 +2,10 @@ package br.ufrn.imd.patterns.identification;
 
 import java.util.HashMap;
 
+import br.ufrn.imd.middleware.annotations.Delete;
 import br.ufrn.imd.middleware.annotations.Get;
 import br.ufrn.imd.middleware.annotations.Post;
+import br.ufrn.imd.middleware.annotations.Put;
 import br.ufrn.imd.middleware.annotations.RequestMap;
 
 public class Lookup {
@@ -17,7 +19,6 @@ public class Lookup {
 		if (clazz.isAnnotationPresent(RequestMap.class)) {
 			RequestMap classAnno = clazz.getAnnotation(RequestMap.class);
 			String baseRoute = classAnno.router();
-			
 			for (java.lang.reflect.Method method : clazz.getDeclaredMethods()) {
 				if(method.isAnnotationPresent(Get.class)) {
 					Get getAnno = method.getAnnotation(Get.class);
@@ -28,6 +29,16 @@ public class Lookup {
 	                Post postAnno = method.getAnnotation(Post.class);
 	                String route = baseRoute + postAnno.router();
 	                System.out.println("Registering POST route: " + route);
+	                registerRouteForMethod(clazz, route, port);
+	            } else if (method.isAnnotationPresent(Put.class)) {
+	            	Put putAnno = method.getAnnotation(Put.class);
+	                String route = baseRoute + putAnno.router();
+	                System.out.println("Registering PUT route: " + route);
+	                registerRouteForMethod(clazz, route, port);
+	            } else if (method.isAnnotationPresent(Delete.class)) {
+	            	Delete deleteAnno = method.getAnnotation(Delete.class);
+	                String route = baseRoute + deleteAnno.router();
+	                System.out.println("Registering DELETE route: " + route);
 	                registerRouteForMethod(clazz, route, port);
 	            }
 			}
